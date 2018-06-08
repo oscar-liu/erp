19c308213f186160616040213c1272084df06017
chencong <1009129020@qq.com>
2018-06-09 00:39:57 +0800

    库存不足通知

--- whalegoods-erp/whalegoods-erp/src/main/java/com/whalegoods/job/PathStockJob.java
+++ whalegoods-erp/whalegoods-erp/src/main/java/com/whalegoods/job/PathStockJob.java
@@ -7,48 +7,44 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.whalegoods.constant.ConstSysParamValue;
-import com.whalegoods.entity.Device;
 import com.whalegoods.service.DeviceService;
 import com.whalegoods.util.SmsUtil;
 
+
 /**
- * 根据上报时间查询离线的柜机
+ * 库存不足通知
  * @author henrysun
- * 2018年6月3日 下午9:08:21
+ * 2018年6月8日 下午4:38:09
  */
-public class DeviceStatusJob implements BaseJob{
+public class PathStockJob implements BaseJob{
 	
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@Autowired
 	private DeviceService deviceService;
 	
-    public DeviceStatusJob() {}
+    public PathStockJob() {}
   
     public void execute(JobExecutionContext context) {
-		Long beforeTime = System.currentTimeMillis()-ConstSysParamValue.DEVICE_OFFLINE_TIME;
-		List<Device> listOff=deviceService.selectListOfOffLine(beforeTime);
-		logger.info("查询到的离线设备：{}",listOff.toString());
-		if(listOff.size()>0){
+    	List<String> oosDeviceList=deviceService.selectOosDevice();
+    	logger.info("查询到的缺货设备：{}",oosDeviceList.toString());
+    	if(oosDeviceList.size()>0){
 			//任务名称必须和后台配置的一样，否则无法获取收件人列表
-			String jobName="设备离线报警";
-			//设备状态更新为离线
-			deviceService.updateBatch(listOff);
+			String jobName="库存不足通知";
 			//发送内容
 			StringBuffer sb=new StringBuffer();
-			for (Device device : listOff) {
-				sb.append(device.getShortName());
+			for (String shortName : oosDeviceList) {
+				sb.append(shortName);
 				sb.append(",");
 			}
 			String deviceNameArr = sb.toString().substring(0,sb.toString().length() - 1);
 			//邮件模板
-			String templateId="SMS_136860073";
+			String templateId="SMS_136870748";
 			//邮件内容
 			String content="{\"device_name\":\""+deviceNameArr+"\"}";
 			//发送邮件
 			SmsUtil.sendSms(jobName,templateId,content);
-		}
+    	}
     }
 
 }
--- whalegoods-erp/whalegoods-erp/src/main/java/com/whalegoods/util/SmsUtil.java
+++ whalegoods-erp/whalegoods-erp/src/main/java/com/whalegoods/util/SmsUtil.java
@@ -1,7 +1,13 @@
 package com.whalegoods.util;
 
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.aliyuncs.DefaultAcsClient;
 import com.aliyuncs.IAcsClient;
@@ -13,16 +19,56 @@ import com.aliyuncs.http.MethodType;
 import com.aliyuncs.profile.DefaultProfile;
 import com.aliyuncs.profile.IClientProfile;
 import com.whalegoods.constant.ConstApiResCode;
+import com.whalegoods.entity.response.ResJobEmail;
 import com.whalegoods.exception.SystemException;
+import com.whalegoods.service.SysUserService;
 
 /**
  * 短信工具类
  * @author henrysun
  * 2018年6月7日 下午5:24:27
  */
+@Component
 public class SmsUtil {
 	
 	private final static Logger logger = LoggerFactory.getLogger(SmsUtil.class);
+	
+	private static SysUserService sysUserService;
+	
+    @Autowired
+    private SysUserService sysUserService2;
+    
+    @PostConstruct
+    public void beforeInit() {
+    	sysUserService = sysUserService2;
+    }
+	
+    /**
+     * 发送短信
+     * jobName 任务名称  
+     * templateId 短信模板
+     * content  短信内容
+     * @author henrysun
+     * 2018年6月9日 上午12:01:07
+     */
+	public static void sendSms(String jobName,String templateId,String content){
+		//查询收件人列表
+		List<ResJobEmail> contrats=sysUserService.getEmailArr(jobName);
+		if(contrats.size()>0&&contrats.get(0).getSwitchStatus()==1){
+			StringBuffer sb2=new StringBuffer();
+			for (ResJobEmail item : contrats) {
+				sb2.append(item.getPhone());
+				sb2.append(",");
+			}
+			String phongArr = sb2.toString().substring(0,sb2.toString().length() - 1);
+			logger.info("查询到的发送短信的手机号：{}",phongArr);
+			try {
+				sendSmsRequest(phongArr,templateId,content);
+			} catch (SystemException e) {
+				logger.error("发送短信失败：{}",e.getMessage());
+			}
+		}
+	}
 	
 	/**
 	 * 批量发送短信
@@ -30,7 +76,7 @@ public class SmsUtil {
 	 * 2018年6月7日 下午5:25:39
 	 * @throws SystemException 
 	 */
-	public static void SendSmsRequest(String phongArr,String deviceNameArr) throws SystemException{
+	private static void sendSmsRequest(String phongArr,String templateId,String content) throws SystemException{
 		System.setProperty("sun.net.client.defaultConnectTimeout","10000");
 		System.setProperty("sun.net.client.defaultReadTimeout","10000");
 		final String product = "Dysmsapi";
@@ -50,8 +96,8 @@ public class SmsUtil {
 		 request.setMethod(MethodType.POST);
 		 request.setPhoneNumbers(phongArr);
 		 request.setSignName("鲸品智能");
-		 request.setTemplateCode("SMS_136860073");
-		 request.setTemplateParam("{\"device_name\":\""+deviceNameArr+"\"}");
+		 request.setTemplateCode(templateId);
+		 request.setTemplateParam(content);
 		SendSmsResponse sendSmsResponse;
 		try {
 			sendSmsResponse = acsClient.getAcsResponse(request);

2922ac4499c5298acf9e591232ea7b2ee781c252
chencong <chencong@dingdang.ai>
2018-06-29 19:16:28 +0800

    柜机异常

---
+++ whalegoods-erp/whalegoods-erp/src/main/java/com/whalegoods/entity/DeviceExLog.java
@@ -0,0 +1,25 @@
+package com.whalegoods.entity;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * 设备异常上报日志devcie_exlog实体类
+ * @author henrysun
+ * 2018年6月29日 下午2:44:25
+ */
+@Getter
+@Setter
+@ToString
+public class DeviceExLog extends BaseEntity implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+    private String orderId;
+    
+    private String errorMessage;
+
+}
